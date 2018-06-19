ea7367604d108a4e6364a2c5ced080b617e9038b
nmassey001 <neil.massey@stfc.ac.uk>
2018-06-19 10:32:20 +0100

    Renaming things back, modified setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='cedatest',
+    name='cedatest_control',
     version='0.1',
-    packages=['cedatest'],
+    packages=['cedatest_control'],
     install_requires=[
         'django',
         'django-sizefield',
