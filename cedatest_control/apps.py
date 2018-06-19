33b7995b4f18421986c37a49a07d4b75931e5d1e
nmassey001 <neil.massey@stfc.ac.uk>
2018-06-19 09:56:38 +0100

    Renamed cedatest_control to cedatest

--- cedatest_control/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CedatestControlConfig(AppConfig):
-    name = 'cedatest_control'
--- cedatest_control/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from cedatest_control import views
-
-urlpatterns = (
-    path('list_users', views.list_users, name='listusers'),
-)
